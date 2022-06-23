dd4e64f687241ba9ef78b3cdca6e3b1fafc85f9b
doniaskima <doniaskima344@gmail.com>
2022-06-23 16:06:18 +0100

    quotes

---
+++ quotes.js
@@ -0,0 +1,257 @@
+const Quotes = [{
+        text: 'Genius is one percent inspiration and ninety-nine percent perspiration.',
+        author: 'Thomas Edison',
+    },
+    {
+        text: 'You can observe a lot just by watching.',
+        author: 'Yogi Berra',
+    },
+    {
+        text: 'A house divided against itself cannot stand.',
+        author: 'Abraham Lincoln',
+    },
+    {
+        text: 'Difficulties increase the nearer we get to the goal.',
+        author: 'Johann Wolfgang von Goethe',
+    },
+    {
+        text: 'Fate is in your hands and no one elses',
+        author: 'Byron Pulsifer',
+    },
+    {
+        text: 'Be the chief but never the lord.',
+        author: 'Lao Tzu',
+    },
+    {
+        text: 'Nothing happens unless first we dream.',
+        author: 'Carl Sandburg',
+    },
+    {
+        text: 'Well begun is half done.',
+        author: 'Aristotle',
+    },
+    {
+        text: 'Life is a learning experience, only if you learn.',
+        author: 'Yogi Berra',
+    },
+    {
+        text: 'Self-complacency is fatal to progress.',
+        author: 'Margaret Sangster',
+    },
+    {
+        text: 'Peace comes from within. Do not seek it without.',
+        author: 'Buddha',
+    },
+    {
+        text: 'What you give is what you get.',
+        author: 'Byron Pulsifer',
+    },
+    {
+        text: 'We can only learn to love by loving.',
+        author: 'Iris Murdoch',
+    },
+    {
+        text: 'Life is change. Growth is optional. Choose wisely.',
+        author: 'Karen Clark',
+    },
+    {
+        text: "You'll see it when you believe it.",
+        author: 'Wayne Dyer',
+    },
+    {
+        text: 'Today is the tomorrow we worried about yesterday.',
+        author: null,
+    },
+    {
+        text: "It's easier to see the mistakes on someone else's paper.",
+        author: null,
+    },
+    {
+        text: 'Every man dies. Not every man really lives.',
+        author: null,
+    },
+    {
+        text: 'To lead people walk behind them.',
+        author: 'Lao Tzu',
+    },
+    {
+        text: 'Having nothing, nothing can he lose.',
+        author: 'William Shakespeare',
+    },
+    {
+        text: 'Trouble is only opportunity in work clothes.',
+        author: 'Henry J. Kaiser',
+    },
+    {
+        text: 'A rolling stone gathers no moss.',
+        author: 'Publilius Syrus',
+    },
+    {
+        text: 'Ideas are the beginning points of all fortunes.',
+        author: 'Napoleon Hill',
+    },
+    {
+        text: 'Everything in life is luck.',
+        author: 'Donald Trump',
+    },
+    {
+        text: 'Nobody will believe in you unless you believe in yourself.',
+        author: 'Liberace',
+    },
+    {
+        text: 'Do more than dream: work.',
+        author: 'William Arthur Ward',
+    },
+    {
+        text: 'No man was ever wise by chance.',
+        author: 'Seneca',
+    },
+    {
+        text: 'Some pursue happiness, others create it.',
+        author: null,
+    },
+    {
+        text: 'He that is giddy thinks the world turns round.',
+        author: 'William Shakespeare',
+    },
+    {
+        text: "Don't ruin the present with the ruined past.",
+        author: 'Ellen Gilchrist',
+    },
+    {
+        text: 'Do something wonderful, people may imitate it.',
+        author: 'Albert Schweitzer',
+    },
+    {
+        text: 'We do what we do because we believe.',
+        author: null,
+    },
+    {
+        text: 'Do one thing every day that scares you.',
+        author: 'Eleanor Roosevelt',
+    },
+    {
+        text: 'If you cannot be silent be brilliant and thoughtful.',
+        author: 'Byron Pulsifer',
+    },
+    {
+        text: 'Who looks outside, dreams; who looks inside, awakes.',
+        author: 'Carl Jung',
+    },
+    {
+        text: 'What we think, we become.',
+        author: 'Buddha',
+    },
+    {
+        text: 'The shortest answer is doing.',
+        author: 'Lord Herbert',
+    },
+    {
+        text: 'All our knowledge has its origins in our perceptions.',
+        author: 'Leonardo da Vinci',
+    },
+    {
+        text: 'The harder you fall, the higher you bounce.',
+        author: null,
+    },
+    {
+        text: 'Trusting our intuition often saves us from disaster.',
+        author: 'Anne Wilson Schaef',
+    },
+    {
+        text: 'Truth is powerful and it prevails.',
+        author: 'Sojourner Truth',
+    },
+    {
+        text: 'Light tomorrow with today!',
+        author: 'Elizabeth Browning',
+    },
+    {
+        text: 'Silence is a fence around wisdom.',
+        author: 'German proverb',
+    },
+    {
+        text: 'Society develops wit, but its contemplation alone forms genius.',
+        author: 'Madame de Stael',
+    },
+    {
+        text: 'The simplest things are often the truest.',
+        author: 'Richard Bach',
+    },
+    {
+        text: 'Everyone smiles in the same language.',
+        author: null,
+    },
+    {
+        text: 'Yesterday I dared to struggle. Today I dare to win.',
+        author: 'Bernadette Devlin',
+    },
+    {
+        text: 'No alibi will save you from accepting the responsibility.',
+        author: 'Napoleon Hill',
+    },
+    {
+        text: 'If you can dream it, you can do it.',
+        author: 'Walt Disney',
+    },
+    {
+        text: 'It is better to travel well than to arrive.',
+        author: 'Buddha',
+    },
+    {
+        text: "Life shrinks or expands in proportion to one's courage.",
+        author: 'Anais Nin',
+    },
+    {
+        text: 'You have to believe in yourself.',
+        author: 'Sun Tzu',
+    },
+    {
+        text: 'Our intention creates our reality.',
+        author: 'Wayne Dyer',
+    },
+    {
+        text: 'Silence is a true friend who never betrays.',
+        author: 'Confucius',
+    },
+    {
+        text: 'Character develops itself in the stream of life.',
+        author: 'Johann Wolfgang von Goethe',
+    },
+    {
+        text: 'From little acorns mighty oaks do grow.',
+        author: 'American proverb',
+    },
+    {
+        text: "You can't stop the waves, but you can learn to surf.",
+        author: 'Jon Kabat-Zinn',
+    },
+    {
+        text: 'Reality does not conform to the ideal, but confirms it.',
+        author: 'Gustave Flaubert',
+    },
+    {
+        text: 'Speak low, if you speak love.',
+        author: 'William Shakespeare',
+    },
+    {
+        text: 'A really great talent finds its happiness in execution.',
+        author: 'Johann Wolfgang von Goethe',
+    },
+    {
+        text: 'Reality leaves a lot to the imagination.',
+        author: 'John Lennon',
+    },
+    {
+        text: 'The greatest remedy for anger is delay.',
+        author: 'Seneca',
+    },
+    {
+        text: 'Growth itself contains the germ of happiness.',
+        author: 'Pearl Buck',
+    },
+    {
+        text: "You can do what's reasonable or you can decide what's possible.",
+        author: null,
+    },
+];
